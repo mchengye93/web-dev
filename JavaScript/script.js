aac40247966c48bf98f74b6c6b071a305cee0553
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-16 14:14:20 -0700

    loops exercises

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -14,13 +14,23 @@ let newsFeed = [
         username: "Sally",
         timeline: "JavaScript cool!"
     }
-]
+] 
 
 let userNamePrompt = prompt('What is your username?');
 let passwordPrompt = prompt('What is your password?');
 
+function isUserValid(username, password) {
+    for (let i = 0; i < database.length; i++ ) {
+
+        if(username === database[i].username && password === database[i].password) {
+            return true;
+        }
+    }
+    return false;
+}
 function signIn(user,pass) {
-    if(user === database[0].username && pass === database[0].password) {
+ 
+    if(isUserValid(user,pass)) {
         console.log(newsFeed);
     } else {
         alert("Sorry, you do not exist!");
