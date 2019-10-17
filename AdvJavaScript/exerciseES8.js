8b274b9b98211851c74881db9f60cf5ef641bb72
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-16 23:37:44 -0700

    es8 pratice

--- AdvJavaScript/exerciseES8.js
+++ AdvJavaScript/exerciseES8.js
@@ -16,9 +16,17 @@ console.log(turtle);
 console.log(rabbit);
 
 
+turtle = turtle.padStart(8);
+rabbit = rabbit.padStart(8);
+
+
 // #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
 // Read about what the second parameter does in padEnd and padStart
 turtle = turtle.trim().padEnd(9, '=');
+
+'     ||<- Start line'
+'🐢======='
+'       🐇'
 
 
 // #3) Get the below object to go from:
@@ -29,3 +37,5 @@ let obj = {
 }
 // to this:
 'my name is Rudolf the raindeer'
+
+Object.entries(obj).map(value => value.join(" ")).join(' ')
