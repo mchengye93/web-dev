f012f2225e9878dcdefe13f32210ff6a995c0203
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-17 17:00:09 -0700

    robofriend style

--- create-react-app/robofriends/src/Card.js
+++ create-react-app/robofriends/src/Card.js
@@ -5,8 +5,8 @@ const Card = (props) => {
     console.log(props);
     return (
         
-        <div className ="bg-light-green dib br3 pa3 ma2">
-            <img src ={`https://robohash.org/${props.robot.id}?100x100`} alt='robots'/>
+        <div className ="bg-light-green dib br3 pa3 ma2 grow">
+            <img src ={`https://robohash.org/${props.robot.id}?50x50`} alt='robots'/>
             <div>
                 <h2>{props.robot.name}</h2>
                 <p>{props.robot.email}</p>
