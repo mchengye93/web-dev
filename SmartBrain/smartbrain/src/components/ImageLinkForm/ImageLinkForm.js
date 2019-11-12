a751f829da952868025b39c01599e8529b08c2cc
Marbo Cheng Ye <marbochengye@gmail.com>
2019-11-11 21:46:09 -0800

    link form

--- SmartBrain/smartbrain/src/components/ImageLinkForm/ImageLinkForm.js
+++ SmartBrain/smartbrain/src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const ImageLinkForm = () => {
     return (
-        <form>
-        </form>
+        <div>
+            <p className='f3'>
+            This Magic Brain will detect faces in your pictures. Try it!
+            </p>
+            <div>
+                <input  className='f4 padding w-70 center' type='text'/>
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+            </div>
+        </div>
     )
 }
 
